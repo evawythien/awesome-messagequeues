a944390c24cac8ef3cdd8b3d2760d84a938f83ec
Eva <evadvgarcia@gmail.com>
2022-01-05 16:01:20 +0100

    add rabbitmq options

---
+++ AwesomeQueues/AwesomeQueues/Rabbit/RabbitOptions.cs
@@ -0,0 +1,33 @@
+﻿namespace AwesomeQueues.Rabbit
+{
+    /// <summary>
+    /// RabbitMQ configuration.
+    /// </summary>
+    public class RabbitOptions
+    {
+        /// <summary>
+        /// Host name.
+        /// </summary>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Port.
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// The name of the user.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Virtual host.
+        /// </summary>
+        public string VirtualHost { get; set; }
+    }
+}
